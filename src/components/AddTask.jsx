2121da49f53629484fa6ca68a7d812cd6ec8aeb5
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-02-28 21:16:01 +0800

    feat:feat: New keyboard shortcuts to improve UX
    While input is focused
    - press enter key to add new task (input is not empty)
    - press escape key to clear input

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,13 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 import style from './css/AddTask.module.css';
 export default function AddTask({ onAddTask }) {
 	//component state
-	const [count, setCount] = useState(0);
 	const inputRef = useRef(null);
 	//component handler
 
 	const handleFormSubmit = () => {
-		setCount((prevCount) => prevCount + 1);
-		console.log(`count is ${count}`);
 		const task = {
 			title: inputRef.current.value,
 			id: uuidv4(),
@@ -22,11 +19,21 @@ export default function AddTask({ onAddTask }) {
 		inputRef.current.value = '';
 	};
 
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			handleFormSubmit();
+		} else if (event.key === 'Escape') {
+			inputRef.current.value = '';
+		} else {
+			return;
+		}
+	};
+
 	return (
 		<div className={style.add_task_container}>
 			<p>Add to list</p>
 			<div className={style.task_input_form}>
-				<input type='text' ref={inputRef} />
+				<input type='text' ref={inputRef} onKeyDown={handleKeyDown} />
 				<button className='add_task_btn' onClick={handleFormSubmit}>
 					+
 				</button>
