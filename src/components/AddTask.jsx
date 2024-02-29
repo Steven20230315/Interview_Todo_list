d8bcf478bee2b8b2e3cccfe1824acf94c6d3acd7
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-02-29 17:04:59 +0800

    feat: Show info about the keyboard shortcut

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import style from './css/AddTask.module.css';
+import { PiQuestionFill } from 'react-icons/pi';
 export default function AddTask({ onAddTask }) {
 	const inputRef = useRef(null);
+	const [isHovered, setIsHovered] = useState(false);
 
 	const handleFormSubmit = () => {
 		const task = {
@@ -17,6 +19,14 @@ export default function AddTask({ onAddTask }) {
 		inputRef.current.value = '';
 	};
 
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
 	const handleKeyDown = (event) => {
 		if (event.key === 'Enter') {
 			handleFormSubmit();
@@ -29,9 +39,22 @@ export default function AddTask({ onAddTask }) {
 
 	return (
 		<div className={style.add_task_container}>
-			<p>Add to list</p>
+			<p>
+				Add to list
+				<PiQuestionFill
+					title='Press Enter to add task, Press Escape to clear'
+					className={style.hint}
+				/>
+			</p>
+
 			<div className={style.task_input_form}>
-				<input type='text' ref={inputRef} onKeyDown={handleKeyDown} />
+				<input
+					type='text'
+					ref={inputRef}
+					onKeyDown={handleKeyDown}
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+				/>
 				<button className={style.add_task_btn} onClick={handleFormSubmit}>
 					+
 				</button>
