acc2f4dba8a5007ed96808cd229923abae9f85fe
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-02-29 10:25:18 +0800

    refactor: Remove redundant code from `ProgressBar`
    Removed redundant `||0` from `ProgressBar` component. Previously, this
    was used to prevent the display of a `NaN` value when there were no
    tasks. With the updated `calculateProgress` function, which returns 0 if
    `tasks.length` is 0, this behavior is no longer necessary.

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,8 +3,11 @@ import style from './css/ProgressBar.module.css';
 export default function ProgressBar({ progress }) {
 	return (
 		<div className={style.progress_container}>
-			<span>{progress || 0}%</span>
-			<div className={style.progress_bar} style={{ '--width': `${progress}` }}></div>
+			<span>{progress}%</span>
+			<div
+				className={style.progress_bar}
+				style={{ '--width': `${progress}` }}
+			></div>
 		</div>
 	);
 }
