025484d89ae16651469ac95e61ac150dc08f5bf5
Steven20230315 <125541670+Steven20230315@users.noreply.github.com>
2024-02-29 17:19:37 +0800

    refactor: remove unused code

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import style from './css/AddTask.module.css';
 import { PiQuestionFill } from 'react-icons/pi';
 export default function AddTask({ onAddTask }) {
 	const inputRef = useRef(null);
-	const [isHovered, setIsHovered] = useState(false);
 
 	const handleFormSubmit = () => {
 		const task = {
@@ -18,14 +17,6 @@ export default function AddTask({ onAddTask }) {
 			onAddTask(task);
 		}
 		inputRef.current.value = '';
-	};
-
-	const handleMouseEnter = () => {
-		setIsHovered(true);
-	};
-
-	const handleMouseLeave = () => {
-		setIsHovered(false);
 	};
 
 	const handleKeyDown = (event) => {
@@ -49,13 +40,7 @@ export default function AddTask({ onAddTask }) {
 			</p>
 
 			<div className={style.task_input_form}>
-				<input
-					type='text'
-					ref={inputRef}
-					onKeyDown={handleKeyDown}
-					onMouseEnter={handleMouseEnter}
-					onMouseLeave={handleMouseLeave}
-				/>
+				<input type='text' ref={inputRef} onKeyDown={handleKeyDown} />
 				<button className={style.add_task_btn} onClick={handleFormSubmit}>
 					+
 				</button>
